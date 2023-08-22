85c2879f4fbbf552f2a22efe740d09b13cbf27f6
Ірина Заєць <irynazaiets@MacBook-Air-Iryna.local>
2023-08-22 16:10:25 +0300

    Added old-time data

--- read_from_csv_2.py
+++ read_from_csv_2.py
@@ -6,31 +6,35 @@ LIST_WITH_NUMBERS_NEW = []
 LIST_WITH_NUMBERS_OLD = []
 
 
-with open('data/csv_files/old.csv', newline='') as old_file:
-    with open('data/csv_files/new_2.csv', newline='') as new_file:
+with open('data/csv_files/old_1.csv', newline='') as old_file:
+    reader_old = csv.reader(old_file)
+    data_old = list(reader_old)
+    
+with open('data/csv_files/new_2.csv', newline='') as new_file:
+    reader_new = csv.reader(new_file)
+    data_new = list(reader_new)
 
-        reader_old = csv.reader(old_file)
-        data_old = list(reader_old)
 
-        reader_new = csv.reader(new_file)
-        data_new = list(reader_new)
+# for i in data_new:
+#     data = re.sub(' +', ' ', i[0])
+#     only_numbers = re.findall(r'[\d]+[.,\d]+|[\d]*[.][\d]+|[\d]+', data)
+#     # only_numbers = re.findall(r'\b\d+\b', data)
+#     if len(only_numbers)>1 and only_numbers[0].isdigit():
+#         if int(only_numbers[0]) in range(0, 1064, 1) and len(only_numbers)>=6:
+#             LIST_WITH_NUMBERS_NEW.append(only_numbers)
 
-
-for i in data_new:
-    data = re.sub(' +', ' ', i[0])
-    only_numbers = re.findall(r'[\d]+[.,\d]+|[\d]*[.][\d]+|[\d]+', data)
-    # only_numbers = re.findall(r'\b\d+\b', data)
-    if len(only_numbers)>1 and only_numbers[0].isdigit():
-        if int(only_numbers[0]) in range(0, 1064, 1) and len(only_numbers)>=6:
-            LIST_WITH_NUMBERS_NEW.append(only_numbers)
 
 for i in data_old:
-    data = re.sub(' +', ' ', i[0])
-    only_numbers = re.findall(r'[\d]+[.,\d]+|[\d]*[.][\d]+|[\d]+', data)
-    # only_numbers = re.findall(r'\b\d+\b', data)
-    if len(only_numbers)>1 and only_numbers[0].isdigit():
-        if int(only_numbers[0]) in range(0, 1064, 1) and len(only_numbers)>=6:
-            LIST_WITH_NUMBERS_OLD.append(only_numbers)
+    if len(i)>=1:
+        data = re.sub(' +', ' ', i[0])
+        only_numbers = re.findall(r'[\d]+[.,\d]+|[\d]*[.][\d]+|[\d]+', data)
+        # only_numbers = re.findall(r'\b\d+\b', data)
+        if len(only_numbers)>1 and only_numbers[0].isdigit():
+            # if int(only_numbers[0]) in range(0, 1058, 1) and len(only_numbers)>=6:
+            if int(only_numbers[0]) in range(10, 1058, 1) and len(only_numbers)>=4:
+                LIST_WITH_NUMBERS_OLD.append(only_numbers)
+    else:
+        pass
 
 #це якась хуйня
 # with open("data/list_of_numbers.csv", "w+", newline="") as file:
@@ -39,14 +43,15 @@ for i in data_old:
 #         write.writerows(data)
 
 
-dbfile_new = open('data_new.pickle', 'ab')
-pickle.dump(LIST_WITH_NUMBERS_NEW, dbfile_new)                   
-dbfile_new.close()
+# dbfile_new = open('data_new.pickle', 'ab')
+# pickle.dump(LIST_WITH_NUMBERS_NEW, dbfile_new)                   
+# dbfile_new.close()
 
-dbfile_new = open('data_new.pickle', 'rb')   
-db_new = pickle.load(dbfile_new)
+# dbfile_new = open('data_new.pickle', 'rb')   
+# db_new = pickle.load(dbfile_new)
 
 
+# print(LIST_WITH_NUMBERS_OLD)
 
 dbfile_old = open('data_old.pickle', 'ab')
 pickle.dump(LIST_WITH_NUMBERS_OLD, dbfile_old)                   
@@ -55,7 +60,7 @@ dbfile_old.close()
 dbfile_old = open('data_old.pickle', 'rb')   
 db_old = pickle.load(dbfile_old)
 
-print(len(db_new))
+# print(len(db_new))
 print(len(db_old))
 
 # print(len(LIST_WITH_NUMBERS))
